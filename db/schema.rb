112df3f60b96a7a28b39cb8e0c5665387c54a389
odairbr <mateusmpa@gmail.com>
2016-07-07 19:50:57 -0300

    view jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160707222421) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "category"
+    t.string   "company"
+    t.text     "description"
+    t.string   "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
