c65555f9ae2868bc676db9eca06c408bcf69e53d
odairbr <odairbr07@gmail.com>
2016-07-07 21:36:38 -0300

    novo e criacao de vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,21 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new job_params
+    @job.save
+
+    redirect_to @job
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :category, :company, :description, :location)
+  end
+
 end
