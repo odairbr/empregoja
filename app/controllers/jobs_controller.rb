07eb0e1ba8d8961093494c7925f2dbab14841cc3
odairbr <odairbr07@gmail.com>
2016-07-11 21:18:35 -0300

    user edit jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
+    
   end
 
   def new
@@ -17,7 +18,22 @@ class JobsController < ApplicationController
       @companies = Company.all
       render 'new'
     end
+  end
 
+  def edit
+    @job = Job.find(params[:id])
+    @companies = Company.all
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @companies = Company.all
+
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      render 'edit'
+    end
   end
 
   private
