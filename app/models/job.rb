01a0e5da8ac3c452d30f73fe4fd29a19c8756bae
odairbr <odairbr07@gmail.com>
2016-07-15 17:05:39 -0300

    user does not view expired jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,13 @@
 class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
+
+  DAYS_TO_EXPIRE = 90
+
   validates :title, :location, :description, :category, presence: true
 
 
   def expired?
-    created_at < 90.days.ago
+    created_at < DAYS_TO_EXPIRE.days.ago
   end
 end
